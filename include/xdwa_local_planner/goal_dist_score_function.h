dbb31c125207545ecefb263c6edf9b291c15fec0
Shivesh Khaitan <shiveshkhaitan@gmail.com>
2019-02-09 02:01:16 +0530

    Implement goal dist plugin

---
+++ include/xdwa_local_planner/goal_dist_score_function.h
@@ -0,0 +1,41 @@
+//
+// Created by shivesh on 2/7/19.
+//
+
+#ifndef XDWA_LOCAL_PLANNER_GOAL_DIST_SCORE_FUNCTION_H
+#define XDWA_LOCAL_PLANNER_GOAL_DIST_SCORE_FUNCTION_H
+
+#include "tf2/utils.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+
+#include "xdwa_local_planner/xdwa_local_planner.h"
+#include "xdwa_local_planner/trajectory_score_function.h"
+
+namespace xdwa_local_planner {
+class GoalDistScoreFunction : public TrajectoryScoreFunction {
+ public:
+  GoalDistScoreFunction();
+  ~GoalDistScoreFunction();
+
+  void initialize(rclcpp::Node::SharedPtr node,
+                  std::shared_ptr<tf2_ros::Buffer> buffer,
+                  geometry_msgs::msg::PoseStamped::SharedPtr goal,
+                  geometry_msgs::msg::PoseStamped::SharedPtr pose,
+                  std::string costmap_topic,
+                  std::vector<std::vector<double>> footprint) override;
+
+  double scoreTrajectory(std::shared_ptr<Trajectory> tj) override;
+
+ private:
+  double getDist(double x, double y);
+
+  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_sub_;
+
+  double transform_tolerance_;
+};
+}
+
+#endif //XDWA_LOCAL_PLANNER_GOAL_DIST_SCORE_FUNCTION_H
---
+++ plugins/goal_dist_score_function.cpp
@@ -0,0 +1,41 @@
+//
+// Created by shivesh on 2/7/19.
+//
+
+#include "xdwa_local_planner/goal_dist_score_function.h"
+
+namespace xdwa_local_planner {
+GoalDistScoreFunction::GoalDistScoreFunction() :
+    transform_tolerance_(1.0) {}
+
+GoalDistScoreFunction::~GoalDistScoreFunction() {}
+
+void GoalDistScoreFunction::initialize(rclcpp::Node::SharedPtr node,
+                                       std::shared_ptr<tf2_ros::Buffer> buffer,
+                                       geometry_msgs::msg::PoseStamped::SharedPtr goal,
+                                       geometry_msgs::msg::PoseStamped::SharedPtr pose,
+                                       std::string costmap_topic,
+                                       std::vector<std::vector<double>> footprint) {
+  scale_ = 1;
+  node_ = node;
+  buffer_ = buffer;
+  tfl_ = std::make_shared<tf2_ros::TransformListener>(*buffer_);
+  goal_ = goal;
+  pose_ = pose;
+}
+
+double GoalDistScoreFunction::scoreTrajectory(std::shared_ptr<xdwa_local_planner::Trajectory> tj) {
+  double cost = 0;
+  for (int i = tj->num_points_scored_ + 1; i <= tj->num_points_; ++i) {
+    cost += std::max(1 / getDist(tj->x_[i], tj->y_[i]), 1e-7);
+    tj->num_points_scored_++;
+  }
+  return cost;
+}
+
+double GoalDistScoreFunction::getDist(double x, double y) {
+  return hypot(goal_->pose.position.x - x, goal_->pose.position.y - y);
+}
+}
+
+PLUGINLIB_EXPORT_CLASS(xdwa_local_planner::GoalDistScoreFunction, xdwa_local_planner::TrajectoryScoreFunction)
